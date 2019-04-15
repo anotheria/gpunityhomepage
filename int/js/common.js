d9e2b1a0e0535283be225e61f86489dab2d51035
Sembler <spsembler@gmail.com>
2019-04-15 11:35:11 +0300

    scroll navigation

--- int/js/common.js
+++ int/js/common.js
@@ -5,3 +5,19 @@ $(window).scroll(function() {
         $("#header").removeClass("top-nav-collapse");
     }
 });
+
+function goToByScroll(id){
+    // Reove "link" from the ID
+    id = id.replace("link", "");
+    // Scroll
+    $('html,body').animate({
+            scrollTop: $("#"+id).offset().top},
+        'slow');
+}
+
+$(".scroll-links a").click(function(e) {
+    // Prevent a page reload when a link is pressed
+    e.preventDefault();
+    // Call the scroll function
+    goToByScroll($(this).attr("id"));
+});
